e4c41a856961d8b95d52be37fe5fc015a790c5bb
Drschemer <ohmtopten@gmail.com>
2022-08-15 15:43:38 +0700

    add insert / update / delete function

--- index.js
+++ index.js
@@ -8,17 +8,45 @@ var connection = mysql.createConnection({
   database : 'nodejs'
 });
  
-connection.connect();
+connection.connect(function(err) {
+  if (err) throw err;
+});
+//Select
+connection.query('SELECT * FROM tbl_node', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The user name is: ', results);
+
+});
+//insert
+var sqlinsert = "insert into tbl_node (f_name) values ('Joe Doe')";
+connection.query(sqlinsert , function (error, results) {
+  if (error) throw error;
+  console.log('insert complete', results);
+});
+//update
+var sqlupdate = "update tbl_node set f_name = 'sunee8' where f_id = 3";
+connection.query(sqlupdate , function (error, results) {
+  if (error) throw error;
+  console.log('update complete', results);
+});
+//delete
+var sqldelete = "delete from tbl_node where f_name = 'sunee8'";
+connection.query(sqldelete , function (error, results) {
+  if (error) throw error;
+  console.log('delete complete', results);
+});
 
 http.createServer( (request, response) => {
     // query
 
-    connection.query('SELECT * FROM tbl_node', function (error, results, fields) {
-        if (error) throw error;
-        console.log('The user name is: ', results[0]);
-      });
+ 
        
-      connection.end();
+      
+
+
+     
+
+     //
 
     //
   response.writeHead(200, {'Content-Type': 'text/plain'});
@@ -26,4 +54,7 @@ http.createServer( (request, response) => {
 }).listen(6969);
 
 
+
+
+connection.end();
 console.log('Server running at http://127.0.0.1:6969/');
